cfa2ded8ffa3e9f4dae21d8e50cde7e7e18eb2d2
Pankaj Rathi <pankajrathi708@gmail.com>
2024-08-30 18:28:01 -0400

    Modified the url link

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import Home from '../pages/Home';
 import AboutUs from '../pages/AboutUs';
 import Testimonials from '../pages/Testimonials';
